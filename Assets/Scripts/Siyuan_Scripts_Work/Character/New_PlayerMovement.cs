eb2ad371ee22ab43ad7b8b5d4e554275e3b5dff4
Siyuan452 (SAE perth) <1018131@student.sae.edu.au>
2023-07-27 18:37:05 +0800

    Updated the New_Player_Movement Script
    Added speed up function and speed up cooling function.

--- Assets/Scripts/Siyuan_Scripts_Work/Character/New_PlayerMovement.cs
+++ Assets/Scripts/Siyuan_Scripts_Work/Character/New_PlayerMovement.cs
@@ -6,10 +6,17 @@ public class New_PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     public float moveSpeed = 5f;
+    public float moveSpeed1;
+    public float moveSpeed2;
+    public float speedUpTimer;
+    public float speedUpPeriod = 1f;
+    public float speedUpCoolingTime = 3f;
 
     void Start()
     {
-        
+        speedUpTimer = 0;
+        moveSpeed1 = moveSpeed;
+        moveSpeed2 = 2f * moveSpeed;
     }
 
     void Update()
@@ -22,7 +29,54 @@ public class New_PlayerMovement : MonoBehaviour
         float _horizontal = Input.GetAxis("Horizontal");
         float _vertical = Input.GetAxis("Vertical");
 
+        SpeedUpFunction();
+
         Vector3 _movement = new Vector3(_horizontal, 0, _vertical);
         transform.Translate(_movement * -1 * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    void SpeedUpFunction()
+    {
+        if (Input.GetKeyDown("space") && speedUpTimer == 0)
+        {
+            speedUpTimer += Time.deltaTime;
+            moveSpeed = moveSpeed2;
+        }
+        else
+        {
+
+        }
+
+        if (speedUpTimer <= speedUpPeriod && speedUpTimer > 0)
+        {
+            speedUpTimer += Time.deltaTime;
+        }
+        else
+        {
+
+        }
+
+        if(speedUpTimer > speedUpPeriod)
+        {
+            speedUpTimer = -speedUpCoolingTime;
+            moveSpeed = moveSpeed1;
+        }
+        else if (speedUpTimer < 0 )
+        {
+            speedUpTimer += Time.deltaTime; 
+        }
+        else
+        {
+
+        }
+
+        if (speedUpTimer < 0 && speedUpTimer >= -0.1f)
+        {
+            speedUpTimer = 0;
+        }
+        else
+        {
+
+        }
     }
 }
