c8215a85a6c69db415e458efd371d73cd43aa9c6
Matt-Hopkins <1027615@student.sae.edu.au>
2023-08-03 15:19:16 +0800

    Level three environment designed

---
+++ Assets/Scripts/BD_WW_EnemyAI_MH.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BD_WW_EnemyAI_MH : MonoBehaviour
+{
+    public Vector3 destination;
+    public Transform target;
+    public GameObject player;
+    public Vector2 spawnRange;
+    public GameObject enemyPrefab;
+    Animator animator;
+
+    private void Start()
+    {
+        destination = player.transform.position;
+        NavigateToDestination(destination);
+
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void NavigateToDestination(Vector3 destination)
+    {
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = destination;
+    }
+
+    void Update()
+    {
+         destination = player.transform.position;
+         NavigateToDestination(destination);
+
+         animator.SetBool("IsMoving", true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Sword")
+        {
+            Debug.Log ("The enemy hit the the sword");
+
+            //EnemyDeath();
+
+            Destroy (this.gameObject);
+        }
+    }
+}
